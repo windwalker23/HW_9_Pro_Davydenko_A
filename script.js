32688c956b995eb332b8bb939aaf33ce724223ec
unknown <yeskennyt@gmail.com>
2022-06-20 10:11:28 +0300

    v1.2

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ deck = new Array(52);
 cardRank = [2, 3, 4, 5, 6, 7, 8, 9, 10, `J`, `Q`, `K`, `T`];
 cardSuit = [`images/clubs.svg`, `images/spades.svg`, `images/diamonds.svg`, `images/hearts.svg`];
 cardPerson = [`images/jack.svg`, `images/queen.svg`, `images/king.svg`];
-variable = "";
 
 for(i = 0, j = 0, k = -1; i < deck.length; i++) {
-    if(i > 0 && i % 4 === 0){
+    if(i > 0 && i % 4 === 0) {
         j++;
         k = 0;
     } else {
         k++;
     }
 
+    variable = "";
     if(j > 8 && j <= 11) {
         variable = `<img class="person" src="${cardPerson[+(j) - 9]}" alt="person">`;
     } else if(j > 11) {
